7a4c32f487324066fb3c82591cf8e9ff356deda0
cihanuelker <cihan.uelker@web.de>
2024-08-23 00:13:07 +0200

    feat(): styled InputFile as drag and drop area

--- PdfMerger/Data/UploadedFile.cs
+++ PdfMerger/Data/UploadedFile.cs
@@ -2,11 +2,7 @@
 
 public class UploadedFile
 {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public string FileName { get; set; }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public string FileName { get; set; } = String.Empty;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public byte[] FileContent { get; set; }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public byte[] FileContent { get; set; } = Array.Empty<byte>();
 }
